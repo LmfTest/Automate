443d77dd57336392307db31034ce4930d13c5c6a
lmf <lmf@truesightai.com>
2022-10-27 15:16:07 +0800

    1.优化log模块，不是最优解

--- API_test/test_run_case.py
+++ API_test/test_run_case.py
@@ -17,48 +17,36 @@ class Test_runCase():
     @pytest.mark.parametrize('args',YamlUtil().read_yaml(os.path.join(case_dir, "testcase.yaml")))
     # @pytest.mark.parametrize('args',YamlUtil(os.getcwd()+"/../API_test_case/").read_case())
     # @pytest.mark.parametrize('args',YamlUtil(case_dir).read_case())
-    @log1(config_path)
-    def test_run(self,args,setup_and_down):
 
-        print(args)
-        print(3333333)
+    @set_request_info(1)
+    @log(config_path)
+    def test_run(self,args):
+
         allure.dynamic.title(args['name'])
         allure.dynamic.description = ('描述')
 
         #用例状态,默认失败
-        #case_status = 0
-        #添加日志
-        # 读取日志配置
-        log_info = YamlUtil().read_yaml(config_path)
-        log = Logger(log_info['log'])
+
         #实例化请求对象
         req = Automate_request(args)
         #添加操作步骤
         with allure.step(args['name']):
             r = req.request_run()
-            res = r.json()
-            log.logger.info(f"请求响应: 状态码:{r.status_code},返回值:{res}")
-            log.logger.info(f"预期结果:{args['validate']}")
-            log.logger.info(f"实际结果:{res}")
-
-
-        # print(args['validate']['eq'])
-
+            args['response'] = r
 
         #断言
         try:
             assert str(args['validate']['eq']) in r.text
-            case_status = 1
-            log.logger.info(f"{args['name']}: 执行成功！")
-            # log.teardown()
+            args['case_status'] = 1
 
         except:
+            args['case_status'] = 0
+            # pytest.fail("用例指执行失败")
+            # pytest.xfail("用例指执行失败")
 
-            log.logger.info(f"{args['name']}: 执行失败！")
-            log.teardown()      #pytest.fail会让用例直接结束，所以log.teardown要在此之前
-            pytest.fail("用例指执行失败")
 
-        log.teardown()
+
+
 
 
 
--- conftest.py
+++ conftest.py
@@ -5,35 +5,6 @@ from Basic.log_module import *
 import decorator
 # 定义一个全局变量，用于存储内容
 global_data = {}
-test_args = {}
-@pytest.fixture(scope = "function")
-def setup_and_down(args,get_test_data):
-    """
-    前置函数
-    """
-    case_name = args['name']
-    args['request']['host'] = get_test_data['host']
-    # 读取日志配置
-    log_info = YamlUtil().read_yaml(config_path)
-    # print(log_info['log'])
-
-    log = Logger(log_info['log'])
-    log.logger.info(f'<{case_name}>用例执行开始')
-    log.logger.info(f"接口路径: {args['request']['path']}")
-    log.logger.info(f"用例名称: {args['name']}")
-    log.logger.info(f"接口地址: {args['request']['host']}{args['request']['path']}")
-    log.logger.info(f"请求头(headers): {args['request']['headers']}")
-    log.logger.info(f"请求入参: {args['request']['data']}")
-    log.teardown()
-    yield
-    """
-    后置函数
-    """
-    log = Logger(log_info['log'])
-    log.logger.info(f'<{case_name}>用例执行结束')
-    log.logger.info("####################")
-    log.teardown()
-
 
 @pytest.fixture(scope = "session",autouse=True)
 def session_setup_down():
@@ -92,32 +63,73 @@ def pytest_addoption(parser):
     )
 
 #获取环境配置
-@pytest.fixture(scope= "session")
+@pytest.fixture(scope = "session")
 def get_env(request):
     """从配置对象中读取自定义参数的值"""
     return request.config.getoption('env')
 
 #设置环境
-@pytest.fixture(scope= "session")
+@pytest.fixture(scope = "session",autouse=True)
 def get_test_data(get_env):
     #读取所有的环境配置
     temp_data = YamlUtil().read_yaml(test_data_path)
     try:
         test_date = temp_data[get_env]
+        global_data['test_data'] = test_date
     except:
         print('环境配置错误')
 
-    return test_date
-
-def log1(file):
-    print(file)
+    # return test_date
+#log装饰器
+def log(type):
     def inner(func):
         def infunc(*args):
-            print("执行log函数开始")
-            print(args)
             args = args[1:]
-            print(args)
+            log_args = args[1]
+            case_name = log_args['name']
+            # 读取日志配置
+            log_info = YamlUtil().read_yaml(config_path)
+            log = Logger(log_info['log'])
+            log.logger.info(f'<{case_name}>用例执行开始')
+            log.logger.info(f"接口路径: {log_args['request']['path']}")
+            log.logger.info(f"用例名称: {log_args['name']}")
+            log.logger.info(f"接口地址: {log_args['request']['host']}{log_args['request']['path']}")
+            log.logger.info(f"请求头(headers): {log_args['request']['headers']}")
+            log.logger.info(f"请求入参: {log_args['request']['data']}")
+            log.teardown()
+
+
             func(*args)
-            print("执行log函数结束")
+
+
+            log = Logger(log_info['log'])
+            log.logger.info(f"请求响应: 状态码:{log_args['response'].status_code},返回值:{log_args['response'].text}")
+            log.logger.info(f"预期结果:{log_args['validate']}")
+            log.logger.info(f"实际结果:{log_args['response'].text}")
+            if (log_args['case_status']):
+                log.logger.info(f"{log_args['name']}: 执行成功！")
+            else:
+                log.logger.info(f"{log_args['name']}: 执行失败！")
+
+            log.logger.info(f'<{case_name}>用例执行结束')
+            log.logger.info("####################")
+            log.teardown()
         return decorator.decorator(infunc,func)
     return inner
+
+
+#设置请求参数装饰器
+def set_request_info(type):
+    def inner(func):
+        def wrapper(*args):
+            args = args[1:]
+            #设置请求的host
+            args[1]['request']['host'] =global_data['test_data']['host']
+            func(*args)
+            #判断用例状态
+            if ( args[1]['case_status']==0):
+                pytest.fail("用例指执行失败")
+
+        return decorator.decorator(wrapper,func)
+    return inner
+
