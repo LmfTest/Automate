d65114c43b9851d25817794f8080bb903219844a
lmf <lmf@truesightai.com>
2022-10-27 15:31:50 +0800

    1.增加命令行测试环境参数env，可设置测试环境

--- runfile.py
+++ runfile.py
@@ -2,14 +2,18 @@ import pytest
 import os
 import sys
 from Basic.get_path import *
-
+import argparse
 if __name__ == '__main__':
-    try:
-        sys_args = sys.argv[1]
-        if sys_args:
-            env = sys_args
-    except:
-        env = 'dev'
+    # 设置执行环境dev为测试，pro为正式，pre为预发布
+    # 之后只需要在运行run_interface.py文件时增加一个参数(python run_interface.py --env=pro),修改env参数就可以切换对应环境
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--env",
+        dest="env",
+        action="store",
+        default="dev", )
+    args = parser.parse_args()
+    env = args.env
 
     # pytest.main(['-vs'])
     # pytest.main(['-vs','-n 2','--reruns=2' ,'ios_testcase/test_login.py','ios_testcase/test_createOrder.py'])
